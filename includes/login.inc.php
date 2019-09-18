69557391debc7d377adaeebd18a975b2dfb8dc6f
culafic <49117297+culafic@users.noreply.github.com>
2019-09-18 17:53:42 +0200

    Add files via upload

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -4,41 +4,44 @@ if (isset($_POST['login-submit'])) {
 
     require 'dbh.inc.php';
 
-    $mailuid = $_POST['mailuid'];
+    $uid = $_POST['mailuid'];
     $password = $_POST['pwd'];
 
-    if (empty($mailuid) or empty($password)) {
-        header('Location: ../index.php?error=emptyfields');
+    if (empty($uid) or empty($password)) {
+        header('Location: ../login.php?error=emptyfields');
+        exit();
+    } elseif (!filter_var($uid, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $uid)) {
+        header("Location: ../login.php?error=invaliduid");
         exit();
     } else {
         $sql = 'SELECT * FROM users WHERE uidUsers=? or emailUsers=?;';
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header('Location: ../index.php?error=sqlerror');
+            header('Location: ../login.php?error=sqlerror');
         } else {
-            mysqli_stmt_bind_param($stmt, 'ss', $mailuid, $mailuid);
+            mysqli_stmt_bind_param($stmt, 'ss', $uid, $uid);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
 
             if ($row = mysqli_fetch_assoc($result)) {
                 $pwdCheck = password_verify($password, $row['pwdUsers']);
                 if ($pwdCheck == false) {
-                    header('Location: ../index.php?erro=wrongpwd');
+                    header('Location: ../login.php?error=wrongpwd');
                     exit();
-                } elseif ($pwdCheck == true) { 
+                } elseif ($pwdCheck == true) {
                     session_start();
                     $_SESSION['userId'] = $row['idUsers'];
-                    $_SESSION['userUid'] = $row['userUid'];
+                    $_SESSION['userUid'] = $row['uidUsers'];
 
-                    header('Location: ../index.php?login=success');
+                    header('Location: ../login.php?login=success');
                     exit();
                 }
             } else {
-                header('Location: ../index.php?error=nouser');
+                header('Location: ../login.php?error=nouser');
             }
         }
     }
 } else {
-    header("Location: ../index.php");
+    header("Location: ../login.php");
     exit();
 }
